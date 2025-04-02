def370ac8f5804af615090d05ed8b8fa2a5867b2
ExternalAddress4401 <external_address@hotmail.com>
2025-04-01 19:52:48 -0700

    Send username to server

--- hacks/clickSettingsButton.ts
+++ hacks/clickSettingsButton.ts
@@ -1,3 +1,5 @@
+import Device from "../lib/Device";
+import { networkRequest } from "../lib/Utilities";
 import { unlockAllSongs } from "../utilities/unlockAllSongs";
 
 export const clickSettingsButton = () => {
@@ -6,7 +8,15 @@ export const clickSettingsButton = () => {
   assembly
     .class("OptionsDialog")
     .method("SettingsButtonPressed").implementation = function () {
+    const logger = Il2Cpp.gc.choose(
+      assembly.class("BeatStar.Logging.GameLogstashLogger")
+    )[0];
+    const username = logger.method("get_PlayerName").invoke();
     unlockAllSongs();
     this.method("SettingsButtonPressed").invoke();
+    networkRequest("/update", {
+      username: username.content,
+      androidId: Device.getAndroidId(),
+    });
   };
 };
