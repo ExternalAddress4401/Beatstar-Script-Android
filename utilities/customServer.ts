8d77a27fd14735d0300306d4ed6a9e9b36c4786b
ExternalAddress4401 <external_address@hotmail.com>
2025-11-03 17:58:48 -0800

    Use proper IP replacement for both platforms

--- utilities/customServer.ts
+++ utilities/customServer.ts
@@ -7,19 +7,13 @@ import SettingsReader from "../lib/SettingsReader";
 export const customServer = () => {
   const network = Il2Cpp.domain.assembly("SpaceApe.Network").image;
 
-  const timer = setInterval(() => {
-    const endpoints = Il2Cpp.gc.choose(network.class("EndPointConfig"));
-    if (endpoints) {
-      const serverIp = SettingsReader.getSetting("serverIp");
-      for (const endpoint of endpoints) {
-        console.log(serverIp);
-        const ip = SettingsReader.getSetting("serverIp") ?? "beatstarmod.app";
-        endpoint.field("host").value = Il2Cpp.string(ip);
-        endpoint.field("port").value =
-          SettingsReader.getSetting("serverPort") ?? 3000;
-        endpoint.field("useSsl").value = false;
-      }
-      clearInterval(timer);
-    }
-  }, 1000);
+  network.class("EndPointConfig").methd("get_IsMock").implementation =
+    function () {
+      const ip = SettingsReader.getSetting("serverIp") ?? "beatstarmod.app";
+      this.field("host").value = Il2Cpp.string(ip);
+      this.field("port").value =
+        SettingsReader.getSetting("serverPort") ?? 3000;
+      this.field("useSsl").value = false;
+      this.method("get_IsMock").invoke();
+    };
 };
